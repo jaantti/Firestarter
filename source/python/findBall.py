2615db3aa80e48a11e03e4eb81891b93612aa878
Antti Jaaniste <jaantti@gmail.com>
2012-09-05 17:33:52 +0300

    Pictures work, video don't

--- source/python/findBall.py
+++ source/python/findBall.py
@@ -2,11 +2,27 @@
 
 import SimpleCV
 
-img = SimpleCV.Image("pic.png")
+'''img = SimpleCV.Image("pic.png")
 
-img2 = img.colorDistance((243, 55, 9)).invert()
-blob = img.findBlobs(60)
+img2 = img.colorDistance((254, 55, 19))
+img3 = img2.binarize(65)
+blob = img3.findBlobs()
+blob2 = blob.filter([b.isCircle(0.2) for b in blob])
 
-blob.show(width=5)
+print blob2.area()
+print blob.coordinates()
+blob2.show(width=5)
 
-img2.save("pic2.png")
+img3.save("pic2.png")'''
+
+cam = SimpleCV.Camera(0)
+disp = SimpleCV.Display()
+
+while(disp.isNotDone()):
+    img = cam.getImage().colorDistance((254, 55, 19)).binarize(65)
+    circ = img.findBlobs()
+    #img.show(width=5)
+    blob = circ.filter([b.isCircle(0.2) for b in blob])
+    #blob.show(width=5)
+    img.drawCircle((blob[-1].x, blob[-1].y), blob[-1].radius(),SimpleCV.Color.BLUE,3)
+    img.show()

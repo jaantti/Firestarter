e909e30482dc1db78fb6a951c122bdffe53f00d1
Antti Jaaniste <jaantti@gmail.com>
2012-11-25 01:07:05 +0200

    changed script

--- calibrated.sh
+++ calibrated.sh
@@ -1,7 +1,9 @@
 
-v4l2-ctl -d /dev/video1 -c red_balance=131
-v4l2-ctl -d /dev/video1 -c blue_balance=145
-v4l2-ctl -d /dev/video1 -c exposure=140
-v4l2-ctl -d /dev/video1 -c main_gain=15
+v4l2-ctl -d /dev/video1 -c red_balance=128
+v4l2-ctl -d /dev/video1 -c blue_balance=128
+v4l2-ctl -d /dev/video1 -c exposure=138
+v4l2-ctl -d /dev/video1 -c main_gain=0
 v4l2-ctl -d /dev/video1 -c sharpness=0
+v4l2-ctl -d /dev/video1 -c hflip=1
+v4l2-ctl -d /dev/video1 -c vflip=1
 python currentcode.py

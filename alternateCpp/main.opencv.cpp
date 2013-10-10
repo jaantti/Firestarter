12bd1bf4a028b510dfbe02b519ea5ba28bcb0b72
Antti Jaaniste <jaantti@gmail.com>
2013-10-10 22:05:15 +0300

    Fixed OpenCV-CMVision mixup

--- alternateCpp/main.opencv.cpp
+++ alternateCpp/main.opencv.cpp
@@ -6,8 +6,6 @@
 #include <string.h>
 #include <stdio.h>
 #include <cstdlib>
-#include "capture.h"
-#include "segmentation.h"
 
 using namespace cv;
 using namespace std;
@@ -29,6 +27,18 @@ bool was_close = false;
 int fb[] = {0, 0};
 int spd1 = 0;
 int spd2 = 0;
+
+int orange_tty[] = {8, 138, 43, 17, 255, 255};
+int yellow_tty[] = {34, 196, 92, 41, 255, 165};
+int blue_tty[] = {92, 66, 32, 108, 205, 57};
+int green_tty[] = {61, 138, 53, 73, 255, 210};
+int black_tty[] = {63, 83, 0, 79, 143, 95};
+
+int orange_t4[] = {6, 198, 193, 18, 255, 255};
+int yellow_t4[] = {23, 236, 171, 27, 255, 255};
+int blue_t4[] = {0, 0, 0, 127, 127, 255};
+int green_t4[] = {35, 136, 0, 55, 255, 255};
+int black_t4[] = {17, 0, 0, 41, 255, 138};
 
 bool init_serial_dev();
 void close_serial();
@@ -54,64 +64,82 @@ int main(){
         cout << buf << endl;
     }*/
 
-	namedWindow("aken");
+    namedWindow("aken");
     namedWindow("aken2");
-    SEGMENTATION segm(640, 480);
+
     VideoCapture capture(0);
-	capture.set(CV_CAP_PROP_FRAME_WIDTH, 640);
-	capture.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
+
     Mat img, img_hsv;
-	int x1, x2, y1, y2;
-    unsigned char *data = NULL;
+
+    RS232_cputs(motor1, "gb\n");
+
     while (true){
+
         capture >> img;
-        data = img.data;
-        segm.readThresholds("conf");
-
-        segm.thresholdImage(data);
-    	segm.EncodeRuns();
-    	segm.ConnectComponents();
-    	segm.ExtractRegions();
-    	segm.SeparateRegions();
-    	segm.SortRegions();
-
-		RS232_cputs(motor1, "gb\n");
-		
-		if(segm.colors[ORANGE].list!=NULL){
-		x1 = segm.colors[ORANGE].list->x1;
-		y1 = segm.colors[ORANGE].list->y1;
-		x2 = segm.colors[ORANGE].list->x2;
-		y2 = segm.colors[ORANGE].list->y2;
-        //cout << segm.colors[ORANGE].list->next << endl;
-		cv::rectangle( img, cv::Point(x1, y1), cv::Point(x2, y2), cv::Scalar(0,0,255) );
-		}
-		/*
-        for(int i = 0; i<segm.colors[ORANGE].num; i++){
-            int x3, y3, x4, y4;
-            segm.colors[ORANGE].list->next;
-            cout<< segm.colors[ORANGE].list->iterator_id<<endl;
-            x3 = segm.colors[ORANGE].list->x1;
-            y3 = segm.colors[ORANGE].list->y1;
-            x4 = segm.colors[ORANGE].list->x2;
-            y4 = segm.colors[ORANGE].list->y2;
-        //cout << segm.colors[ORANGE].list->next << endl;
-			cv::rectangle( img, cv::Point(x3, y3), cv::Point(x4, y4), cv::Scalar(0,0,255) );
-        }*/
-        //cout<<segm.colors[ORANGE].num << endl;
         cvtColor(img, img_hsv, CV_BGR2HSV);
-        imshow("aken", img_hsv);
-        
-        imshow("aken2", img);
+        imshow("aken2", img_hsv);
+        Mat tr_img = thresholdedImg(img_hsv, orange_t4);
+        imshow("aken", tr_img);
 
-        findBall(x2-x1, y2-y1);
+        vector<Point2f> point = findBlobCenter(tr_img, 5.0);
+
+        cout << point[0].x  << ", " << point[0].y << endl;
+        getBall();
+        findBall(point[0].x, point[0].y);
+
 
         if (waitKey(10) == 27) break;
 
     }
 
-
     close_serial();
     return 0;
+}
+
+Mat thresholdedImg(const Mat img, int* colour){
+    //reads an image from the feed and thresholds it using the provided min/max HSV values'''
+    Mat img_thresholded = img.clone();
+
+    inRange(img, Scalar(colour[0], colour[1], colour[2]), Scalar(colour[3], colour[4], colour[5]), img_thresholded);
+
+    //imshow("aken", img_thresholded); //show picture for calibrating
+    return img_thresholded;
+}
+
+vector<Point2f> findBlobCenter(Mat img_thresholded, double minSize){
+    //using a binary image finds the center of the biggest blob and returns it's coordinates as a tuple'''
+
+    // find contours
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+    findContours(img_thresholded, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
+
+    if (!contours.empty()){
+        sort(contours.begin(), contours.end(), compareContourAreas);
+
+        // grab contours
+        vector<Point> biggestContour = contours[contours.size()-1];
+
+        double area = contourArea(Mat(biggestContour));
+
+        if (area >= minSize){
+            vector<Moments> mu(1);
+            mu[0] = moments(biggestContour, false);
+            vector<Point2f> mc(1);
+            mc[0] = Point2f(mu[0].m10/mu[0].m00, mu[0].m01/mu[0].m00);
+            return mc;
+        }
+    }
+
+    vector<Point2f> mc(1);
+    return mc;
+
+}
+
+bool compareContourAreas (vector<Point> contour1, vector<Point> contour2 ) {
+    double i = fabs( contourArea(Mat(contour1)) );
+    double j = fabs( contourArea(Mat(contour2)) );
+    return ( i < j );
 }
 
 void findBall(double x, double y){
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -3,19 +3,14 @@
 #include <unistd.h>
 #include "rs232.h"
 #include <sstream>
-#include <string.h>
-#include <stdio.h>
-#include <cstdlib>
 #include "capture.h"
 #include "segmentation.h"
-
 using namespace cv;
 using namespace std;
 
-int baudrate = 115200;
+int baudrate = 9600;
 int motor1 = 24; //parem
 int motor2 = 25; //vasak   24 /dev/ttyACM0, 25 /dev/ttyACM1
-int coil = 2; // /dev/ttyACM2
 
 double rel_pos = 0;
 int rel_pos_ball = 0;
@@ -30,22 +25,46 @@ int fb[] = {0, 0};
 int spd1 = 0;
 int spd2 = 0;
 
-bool init_serial_dev();
-void close_serial();
-unsigned char *serial_read(int);
+int orange_tty[] = {8, 138, 43, 17, 255, 255};
+int yellow_tty[] = {34, 196, 92, 41, 255, 165};
+int blue_tty[] = {92, 66, 32, 108, 205, 57};
+int green_tty[] = {61, 138, 53, 73, 255, 210};
+int black_tty[] = {63, 83, 0, 79, 143, 95};
+
+int orange_t4[] = {6, 198, 193, 18, 255, 255};
+int yellow_t4[] = {23, 236, 171, 27, 255, 255};
+int blue_t4[] = {0, 0, 0, 127, 127, 255};
+int green_t4[] = {35, 136, 0, 55, 255, 255};
+int black_t4[] = {17, 0, 0, 41, 255, 138};
+
 Mat thresholdedImg(Mat, int*);
 vector<Point2f> findBlobCenter(Mat, double);
 bool compareContourAreas (vector<Point>, vector<Point>);
 void findBall(double, double);
 void write_spd(int write1, int write2);
-char getBall();
 
+string to_string (int Number ){
+    ostringstream ss;
+    ss << Number;
+    return ss.str();
+}
 
 int main(){
 
-    init_serial_dev();
+    //init motors
+   /* if(RS232_OpenComport(motor1, baudrate))
+    {
+        cout << "Can not open /dev/ttyACM0\n";
+        return(0);
+    }
+    if(RS232_OpenComport(motor2, baudrate)){
+        cout << "Can not open /dev/ttyACM1\n";
+        return(0);
+    }
+*/
+
     //RS232_cputs(serialport, "?\n");
-    //Lugemise aeg
+
     /*unsigned char buf[11];
     usleep(100000);
     int n = RS232_PollComport(serialport, buf, 10);
@@ -54,29 +73,27 @@ int main(){
         cout << buf << endl;
     }*/
 
-	namedWindow("aken");
+    namedWindow("aken");
     namedWindow("aken2");
     SEGMENTATION segm(640, 480);
     VideoCapture capture(0);
 	capture.set(CV_CAP_PROP_FRAME_WIDTH, 640);
 	capture.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
     Mat img, img_hsv;
-	int x1, x2, y1, y2;
-    unsigned char *data = NULL;
+    unsigned char *data;
     while (true){
         capture >> img;
         data = img.data;
         segm.readThresholds("conf");
 
-        segm.thresholdImage(data);
+        segm.thresholdImage( data );
     	segm.EncodeRuns();
     	segm.ConnectComponents();
     	segm.ExtractRegions();
     	segm.SeparateRegions();
     	segm.SortRegions();
 
-		RS232_cputs(motor1, "gb\n");
-		
+		int x1, x2, y1, y2;
 		if(segm.colors[ORANGE].list!=NULL){
 		x1 = segm.colors[ORANGE].list->x1;
 		y1 = segm.colors[ORANGE].list->y1;
@@ -85,7 +102,6 @@ int main(){
         //cout << segm.colors[ORANGE].list->next << endl;
 		cv::rectangle( img, cv::Point(x1, y1), cv::Point(x2, y2), cv::Scalar(0,0,255) );
 		}
-		/*
         for(int i = 0; i<segm.colors[ORANGE].num; i++){
             int x3, y3, x4, y4;
             segm.colors[ORANGE].list->next;
@@ -95,23 +111,74 @@ int main(){
             x4 = segm.colors[ORANGE].list->x2;
             y4 = segm.colors[ORANGE].list->y2;
         //cout << segm.colors[ORANGE].list->next << endl;
-			cv::rectangle( img, cv::Point(x3, y3), cv::Point(x4, y4), cv::Scalar(0,0,255) );
-        }*/
+		cv::rectangle( img, cv::Point(x3, y3), cv::Point(x4, y4), cv::Scalar(0,0,255) );
+        }
         //cout<<segm.colors[ORANGE].num << endl;
         cvtColor(img, img_hsv, CV_BGR2HSV);
         imshow("aken", img_hsv);
-        
+        //Mat tr_img = thresholdedImg(img_hsv, orange_t4);
         imshow("aken2", img);
 
-        findBall(x2-x1, y2-y1);
+        //vector<Point2f> point = findBlobCenter(tr_img, 5.0);
+
+        //cout << point[0].x  << ", " << point[0].y << endl;
+        //findBall(point[0].x, point[0].y);
 
         if (waitKey(10) == 27) break;
 
     }
+  //  RS232_CloseComport(motor1);
+    //RS232_CloseComport(motor2);
 
-
-    close_serial();
     return 0;
+}
+
+Mat thresholdedImg(const Mat img, int* colour){
+    //reads an image from the feed and thresholds it using the provided min/max HSV values'''
+    Mat img_thresholded = img.clone();
+
+    inRange(img, Scalar(colour[0], colour[1], colour[2]), Scalar(colour[3], colour[4], colour[5]), img_thresholded);
+
+    //imshow("aken", img_thresholded); //show picture for calibrating
+    return img_thresholded;
+}
+
+vector<Point2f> findBlobCenter(Mat img_thresholded, double minSize){
+    //using a binary image finds the center of the biggest blob and returns it's coordinates as a tuple'''
+
+
+
+    // find contours
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+    findContours(img_thresholded, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
+
+    if (!contours.empty()){
+        sort(contours.begin(), contours.end(), compareContourAreas);
+
+        // grab contours
+        vector<Point> biggestContour = contours[contours.size()-1];
+
+        double area = contourArea(Mat(biggestContour));
+
+        if (area >= minSize){
+            vector<Moments> mu(1);
+            mu[0] = moments(biggestContour, false);
+            vector<Point2f> mc(1);
+            mc[0] = Point2f(mu[0].m10/mu[0].m00, mu[0].m01/mu[0].m00);
+            return mc;
+        }
+    }
+
+    vector<Point2f> mc(1);
+    return mc;
+
+}
+
+bool compareContourAreas (vector<Point> contour1, vector<Point> contour2 ) {
+    double i = fabs( contourArea(Mat(contour1)) );
+    double j = fabs( contourArea(Mat(contour2)) );
+    return ( i < j );
 }
 
 void findBall(double x, double y){
@@ -154,95 +221,4 @@ void write_spd(int write1, int write2){
 
     RS232_cputs(motor1, ss1.str().c_str());
     RS232_cputs(motor2, ss2.str().c_str());
-}
-
-char getBall(){
-    unsigned char buf[11];
-    RS232_PollComport(motor2, buf, 10);
-    RS232_cputs(motor2, "gb\n");
-    return buf[3];
-}
-
-void boom(){
-
-}
-
-unsigned char *serial_read(int id){
-    unsigned char buf[15];
-    int n = RS232_PollComport(id, buf, 15);
-    if (n) {
-        return buf;
-    }
-    else{
-        return 0;
-    }
-}
-
-bool init_serial_dev(){
-
-    if(RS232_OpenComport(motor1, baudrate))
-    {
-        cout << "Can not open /dev/ttyACM0\n";
-        return(false);
-    }
-    if(RS232_OpenComport(motor2, baudrate)){
-        cout << "Can not open /dev/ttyACM1\n";
-        return(false);
-    }
-    if(RS232_OpenComport(coil, baudrate)){
-        cout << "Can not open /dev/ttyACM2\n";
-        return(false);
-    }
-
-    RS232_cputs(motor1, "?\n");
-    unsigned char *m1 = serial_read(motor1);
-    RS232_cputs(motor2, "?\n");
-    unsigned char *m2 = serial_read(motor2);
-    RS232_cputs(coil, "?\n");
-    unsigned char *c0 = serial_read(coil);
-    bool out_status = true;
-
-    if(m1 && m2 && c0){
-        if(m1[4]=='0'){
-            if(m2[4]=='1'){
-                coil = 2;
-            }
-            else{
-                coil = 1;
-                motor2 = 2;
-            }
-        }
-        else if(m1[4] == '1'){
-            if(m2[4] == '0'){
-                motor1 = 1;
-                motor2 = 0;
-                coil = 2;
-            }
-            else{
-                motor1 = 2;
-                motor2 = 0;
-                coil = 1;
-            }
-        }
-        else{
-            if(m2[4] == '1'){
-                motor1 = 2;
-                coil = 0;
-            }
-            else{
-                motor1 = 2;
-                motor2 = 1;
-                coil = 0;
-            }
-        }
-    }
-    else out_status = false;
-
-    return out_status;
-}
-
-void close_serial(){
-    RS232_CloseComport(motor1);
-    RS232_CloseComport(motor2);
-    RS232_CloseComport(coil);
 }
